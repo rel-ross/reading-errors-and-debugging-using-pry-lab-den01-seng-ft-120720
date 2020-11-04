f0485885f4beb2a3433cdbcbc6438113dc9e1ef1
Arielle ross <arielle@arielleross.com>
2020-11-04 03:24:53 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,13 +1,5 @@
 # don't forget to add: require 'pry'
 require 'pry'
-def engage
-  puts state_log(date)
-  date = generate_star_date
-  binding.pry 
-  puts 'final test'
-end
-
-
 def generate_star_date
   (rand(100000) + 400000) / 10.0
   binding.pry
@@ -21,3 +13,9 @@ def state_log(star_date)
   puts 'test state_log'
 end
 
+def engage
+  puts state_log(date)
+  date = generate_star_date
+  binding.pry 
+  puts 'final test'
+end
