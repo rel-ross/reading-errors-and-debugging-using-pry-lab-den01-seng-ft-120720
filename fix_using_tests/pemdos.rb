b2a5c12a9795847a0f40f0ad5ea20b4c89f01d1a
Arielle ross <arielle@arielleross.com>
2020-11-04 03:14:30 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,11 +1,8 @@
 # don't forget to add: require 'pry'
 require 'pry'
 def snake_it_up(string)
-   puts 'start'
- binding.pry
- puts 'stop'
   if string[0] == "s"
-    "ssssssssss" + string
+    "sssssssss" + string
   else
     string
   end
